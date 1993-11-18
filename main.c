b55e546f72ddbc9baacee1ba5c25bcce858138bf
laf <>
1993-11-18 20:33:01 +0000

    added -f option added multiple input files changed output format to include file name (if it has a name)

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 /* $Header$ */
 
 
-#include <stdio.h>	/* for stderr, fprintf, FILE, fopen, fread */
+#include <stdio.h>	/* for stderr, fprintf, FILE, fopen, fread, EOF */
+#include <stdlib.h>	/* for getopt */
 #include "md5.h"
 
 
@@ -10,10 +11,12 @@
 
 #ifdef __STD_C__
 void usage(char *progname);
+void dofile(FILE *infile, char *inname);
 int main(int argc, char **argv);
 
 #else
 void usage();
+void dofile();
 int main();
 
 #endif
@@ -22,43 +25,22 @@ int main();
 void usage(progname)
 char *progname;
 {
-  fprintf(stderr, "usage: %s [input file]\n", progname);
+  fprintf(stderr, "usage: %s [-f filelist] | [input file]...\n", progname);
   fprintf(stderr, "\n");
   fprintf(stderr, "RSA Data Security, Inc. MD5 Message-Digest Algorithm\n");
   fprintf(stderr, "Hash a file using the MD5 algorithm.\n");
   fprintf(stderr, "Output 4 32-bit (8 digit) hexadecimal numbers.\n");
-  fprintf(stderr, "Input from file, if given, or from stdin.\n");
+  fprintf(stderr, "Input from file(s), if given, or from stdin, or from filelist.\n");
 }
 
 
-int main(argc, argv)
-int argc;
-char **argv;
+void dofile(infile, inname)
+FILE *infile;
+char *inname;
 {
-  FILE *infile;
   char buf[BUFSIZE];
   int  numread;
   MD5_CTX mdContext;
-
-  switch (argc) {
-  case 1:
-    infile = stdin;
-    break;
-  case 2:
-    if (argv[1][0] == '-' && argv[1][1] == '?' && argv[1][2] == 0) {
-      usage(argv[0]);
-      exit(1);
-    }
-    if (NULL == (infile = fopen(argv[1], "r"))) {
-      perror("fopen");
-      exit(2);
-    }
-    break;
-  default:
-    usage(argv[0]);
-    exit(1);
-    break;
-  }
 
   MD5Init(&mdContext);
   do {
@@ -67,7 +49,7 @@ char **argv;
   } while (numread > 0);
   MD5Final(&mdContext);
   printf(
-    "%.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x\n",
+    "%.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x %.2x%.2x%.2x%.2x",
     mdContext.digest[ 0],
     mdContext.digest[ 1],
     mdContext.digest[ 2],
@@ -85,6 +67,81 @@ char **argv;
     mdContext.digest[14],
     mdContext.digest[15]
   );
+  if (inname != NULL)
+    printf(": %s", inname);
+  printf("\n");
+}
+
+
+int main(argc, argv)
+int argc;
+char **argv;
+{
+  char *inname, *listname;
+  FILE *infile, *listfile;
+
+  {
+    int c;
+    extern char *optarg;
+    extern int optind, opterr, optopt;
+
+    listname = NULL;
+    while (EOF != (c = getopt(argc, argv, "f:"))){
+      switch (c) {
+      case 'f':
+	listname = optarg;
+	if (listname[0] == '-' && listname[1] == 0) {
+	  listfile = stdin;
+	} else {
+	  if (NULL == (listfile = fopen(listname, "r"))) {
+	    perror("fopen list file");
+	    exit(2);
+	  }
+	}
+	break;
+      case '?':
+	usage(argv[0]);
+	exit(1);
+      }
+    }
+  }
+
+  if (listname == NULL) {
+    if (optind == argc) {
+      dofile(stdin, NULL);
+    } else {
+      for ( ; optind < argc; optind++) {
+	inname = argv[optind];
+	if (inname[0] == '-' && inname[1] == 0) {
+	  infile = stdin;
+	  inname = NULL;
+	} else {
+	  if (NULL == (infile = fopen(inname, "r"))) {
+	    perror("fopen");
+	    exit(2);
+	  }
+	}
+	dofile(infile, inname);
+      }
+    }
+  } else {
+    char inname[1024];
+    int  len;
+    while (NULL != fgets(inname, sizeof(inname), listfile)) {
+      len = strlen(inname);
+      if (inname[len-1] != '\n') {
+	fprintf(stderr, "file name too long\n");
+	exit(2);
+      }
+      inname [len-1] = 0;
+      if (NULL == (infile = fopen(inname, "r"))) {
+	perror("fopen");
+	exit(2);
+      }
+      dofile(infile, inname);
+    }
+  }
+
   exit(0);
 }
 
