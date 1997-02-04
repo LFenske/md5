6e049fb27e7a703580832b52933cb2ed48ec5bb4
larry <>
1997-02-04 05:11:01 +0000

    new md5 include file directly from RFC 1321

--- md5.h
+++ md5.h
@@ -1,26 +1,36 @@
-static char *hSCCSID = "@(#)$Header$";
-/************************************************************
- ** md5.h -- header file for implementation of MD5                    **
-    RSA Data Security, Inc. MD5 Message-Digest Algorithm              **
-    Created: 2/17/90 RLR                                              **
-    Revised: 12/27/90 SRD,AJ,BSK,JT Reference C version               **
-    Revised (for MD5): RLR 4/27/91                                    **
- ***********************************************************/
-/* typedef a 32-bit type */
-typedef unsigned long int UINT4;
+/* MD5.H - header file for MD5C.C
+ */
 
-/* Data structure for MD5 (Message-Digest) computation */
+/* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+rights reserved.
+
+License to copy and use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+or this function.
+
+License is also granted to make and use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software or the suitability of this
+software for any particular purpose. It is provided "as is"
+without express or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this
+documentation and/or software.
+ */
+
+/* MD5 context. */
 typedef struct {
-  UINT4 i[2];                   /* number of _bits_ handled mod 2^64 */
-  UINT4 buf[4];                                    /* scratch buffer */
-  unsigned char in[64];                              /* input buffer */
-  unsigned char digest[16];     /* actual digest after MD5Final call */
+  UINT4 state[4];                                   /* state (ABCD) */
+  UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
+  unsigned char buffer[64];                         /* input buffer */
 } MD5_CTX;
 
-void MD5Init ();
-void MD5Update ();
-void MD5Final ();
-
-/************************************************************
- ** End of md5.h                                                      **
- ***********************************************************/
+void MD5Init PROTO_LIST ((MD5_CTX *));
+void MD5Update PROTO_LIST
+  ((MD5_CTX *, unsigned char *, unsigned int));
+void MD5Final PROTO_LIST ((unsigned char [16], MD5_CTX *));
